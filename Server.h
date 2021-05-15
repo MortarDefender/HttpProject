384b2d40f6b7a07b7cd5031bc62362f930214653
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2021-05-15 20:57:12 +0300

    add class server and enum action

--- Server.h
+++ Server.h
@@ -1,31 +1,34 @@
 #pragma once
 #pragma comment(lib, "Ws2_32.lib")
 #include <winsock2.h>
+#include "HttpProtocol.h"
 
 // Constants
-#define TIME_PORT 27015
+#define PORT 27015
 #define MAX_SOCKETS 60
-#define EMPTY 0
-#define LISTEN 1
-#define RECEIVE 2
-#define IDLE 3
-#define SEND 4
-#define SEND_TIME 1
-#define SEND_SECONDS 2
+// #define BUFFER_SIZE 1024
 
-// Change struct
+enum class Action { EMPTY, LISTEN, RECEIVE, IDLE, SEND };
+
 struct SocketState {
-	SOCKET id;			// Socket handle
-	int	recv;			// Receiving?
-	int	send;			// Sending?
-	int sendSubType;	// Sending sub-type
-	char buffer[128];
+	SOCKET id;
+	Action action;
+	char buffer[BUFFER_SIZE];  // 128
 	int len;
 };
 
+class Server {
+	private:
+		int socketsCount;
+		HttpProtocol* proc;
+		struct SocketState sockets[MAX_SOCKETS] = { 0 };
 
-bool addSocket(SOCKET id, int what);
-void removeSocket(int index);
-void acceptConnection(int index);
-void receiveMessage(int index);
-void sendMessage(int index);
+		bool addSocket(SOCKET id, Action what);
+		void removeSocket(int index);
+		void acceptConnection(int index);
+		void receiveMessage(int index);
+		void sendMessage(int index);
+	public:
+		Server();
+		int serverMain();
+};

66ce7fef08d81982e789a93cf486eb1cd3f0938e
MortarDefender <63960808+MortarDefender@users.noreply.github.com>
2021-05-19 20:57:18 +0300

    Update HttpProtocol.h

--- HttpProtocol.h
+++ HttpProtocol.h
@@ -4,22 +4,25 @@
 #include <iostream>
 using namespace std;
 
-#define DEBUG false
-#define BUFFER_SIZE 1024
+#define PATH "C:\\temp"
+#define DEBUG true
+#define FULL_DEBUG false
+#define BUFFER_SIZE 4096
 
-enum class Protocol { GET, POST, HEAD, OPTION, DEL, PUT, TRACE, Error };
+enum class Protocol { GET, POST, HEAD, OPTIONS, DEL, PUT, TRACE, Error };
 
 class HttpProtocol {
 	private:
-		int request_status; // can be removed and given at each method as a veriable
-		unordered_map<string, vector<string>> allowed_methods; // Mapping to each URL a list of Allowed Methods
+		int request_status;
 		unordered_map<int, string> errorCodes;
+		unordered_map<string, vector<string>> allowed_methods; // Mapping to each URL a list of Allowed Methods
 
 		string day[7] = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 		string month[12] = { "January" , "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
 		
-		unordered_map<string, string> parse(string data, Protocol& method, string& url, string& version);  // string& method
-		string createMessage(string version, int status, string response_body = "");
+		unordered_map<string, string> parse(string data, Protocol& method, string& url, string& version, string& body);
+		unordered_map<string, string> getVariables(string data, string& url, Protocol method);
+		string createMessage(string version, int status, string response_body = "", string optional = "");
 		string getResponse(string request, string version, int status, string parsedUrl, unordered_map<string, string> variables, Protocol method, string response_body="");
 		string getCurrentDate(string format);
 
@@ -27,16 +30,19 @@ class HttpProtocol {
 		vector<std::string> split(std::string data, std::string delimiter);
 		string getDelimiter(string data, string start, string finish="");
 		string intToString(int number);
+		string stringReplace(string data, char old, char newC);
+		bool isAllowed(string url, Protocol method);
 
-		int urlParser(string url, Protocol method, unordered_map<string, string> variables, string& body); // string method
+		int urlParser(string url, Protocol& method, unordered_map<string, string> variables, string& body);
 		string convertUp(string data);
 		string getFromFile(string fileName);
 		void createFile(string fileName, string content);
+		string getErrorHtml(int status);
 
 		string Get(string version, int status, string response_body);
 		string Post(string version, int status, string response_body);
 		string Head(string version, int status);
-		string Option(string version, string parsed_url);
+		string Options(string version, string parsed_url);
 		string Delete(string filename, string version, string parsedUrl);
 		string Put(string fileName, string field, string version, string parsedUrl);
 		string Trace(string request, string version, int status);
